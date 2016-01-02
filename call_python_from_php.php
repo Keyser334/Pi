7d9f559063a38cf919d501970f20ccc2cc399d80
Keyser334 <clyde334@gmail.com>
2016-01-02 09:40:58 -0500

    Update call_python_from_php.php

--- call_python_from_php.php
+++ call_python_from_php.php
@@ -1,3 +1,4 @@
 echo "Hello World from PHP";
 echo '</br>';
-echo exec ('/var/www/html/scripts/test.py');
+echo exec ('/var/www/html/scripts/test.py');//path to script
+add "#!/usr/bin/python" to pything script so it can run without promping for password
